b236935065c8788313d600e2ef2d9b3ad6deb70e
swathi k <swathikathera67@gmail.com>
2023-06-05 23:30:48 -0500

    Enjd of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+
+
+using System.Data.Common;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,30 @@
+using System.Data.Common;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
